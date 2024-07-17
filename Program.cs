d75d278be5192b4254fc6fc32d6ad5936e871a81
danghai2110 <ngo368032@gmail.com>
2024-07-17 19:06:08 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        double a, b;
+        Console.WriteLine("Nhập A: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        if (a == 0)
+        {
+            Console.WriteLine("a phải khác 0");
+
+        }
+        else
+        {
+            Console.WriteLine("Nhập B:");
+            b = Convert.ToDouble(Console.ReadLine());
+            double x = -b / a;
+            Console.WriteLine("{0}x + {1} = 0\n=> x = {2}", a, b, x);
+
+        }
+        Console.WriteLine();
+    }
+}
